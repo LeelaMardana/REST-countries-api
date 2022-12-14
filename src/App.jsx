fa760118f4c8251b8814a890216a04cf638b802f
AwesemoTowa <Deankurumi@gmail.com>
2022-12-14 13:30:52 +0600

    Add UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
-import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import { Header } from './components/Header';
+import { Main } from './components/Main';
+
+import { HomePage } from './pages/HomePage';
+import { Details } from './pages/Details';
+import { NotFound } from './pages/NotFound';
+
 function App() {
-  return <div>123</div>;
+  return (
+    <>
+      <Header />
+      <Main>
+        <Routes>
+          <Route exact path='/' element={<HomePage />} />
+          <Route path='/country/:name' element={<Details />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </Main>
+    </>
+  );
 }
 
 export default App;
