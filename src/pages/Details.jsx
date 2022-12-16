0ccb435abe04e6f30bdde60bbaff4fe30d8ff439
AwesemoTowa <Deankurumi@gmail.com>
2022-12-16 21:27:32 +0600

    fix bug with return

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,23 +1,29 @@
 import { useNavigate, useParams } from 'react-router-dom';
-import {useSelector, useDispatch} from 'react-redux';
-import {useEffect} from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 import { IoArrowBack } from 'react-icons/io5';
 
 import { Button } from '../components/Button';
 import { Info } from '../components/Info';
 import { selectDetails } from '../store/details/details-selector';
-import { loadCountryByName } from '../store/details/details-actions';
-
+import {
+  clearDetails,
+  loadCountryByName,
+} from '../store/details/details-actions';
 
 export const Details = () => {
   const { name } = useParams();
   const dispatch = useDispatch();
-  const {currentCountry, error, status} = useSelector(selectDetails);
+  const { currentCountry, error, status } = useSelector(selectDetails);
 
   const navigate = useNavigate();
 
   useEffect(() => {
     dispatch(loadCountryByName(name));
+
+    return () => {
+      dispatch(clearDetails());
+    };
   }, [name, dispatch]);
 
   return (
--- src/store/details/details-reducer.js
+++ src/store/details/details-reducer.js
@@ -1,4 +1,9 @@
-import { SET_COUNTRY, SET_ERROR, SET_LOADING } from './details-actions';
+import {
+  CLEAR_DETAILS,
+  SET_COUNTRY,
+  SET_ERROR,
+  SET_LOADING,
+} from './details-actions';
 
 const initialState = {
   currentCountry: null,
@@ -26,6 +31,8 @@ export const detailsReducer = (state = initialState, { type, payload }) => {
         status: 'received',
         currentCountry: payload,
       };
+    case CLEAR_DETAILS:
+      return initialState;
     default:
       return state;
   }
