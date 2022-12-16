64c1f4fcb3f89a5d361b42c08f76219a355b4fb5
AwesemoTowa <Deankurumi@gmail.com>
2022-12-16 21:51:41 +0600

    Finish

--- src/store/details/details-reducer.js
+++ src/store/details/details-reducer.js
@@ -3,12 +3,14 @@ import {
   SET_COUNTRY,
   SET_ERROR,
   SET_LOADING,
+  SET_NEIGHBORS,
 } from './details-actions';
 
 const initialState = {
   currentCountry: null,
   status: 'idle',
   error: null,
+  neighbors: [],
 };
 
 export const detailsReducer = (state = initialState, { type, payload }) => {
@@ -33,6 +35,11 @@ export const detailsReducer = (state = initialState, { type, payload }) => {
       };
     case CLEAR_DETAILS:
       return initialState;
+    case SET_NEIGHBORS:
+      return {
+        ...state,
+        neighbors: payload,
+      };
     default:
       return state;
   }
